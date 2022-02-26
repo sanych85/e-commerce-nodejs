ab193b9bd375f9bc850143d320ad02280b7fdd91
Aleks Shatokhin <aleks_shatoh@mail.ru>
2022-02-26 11:43:45 +0300

    set up project

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,22 @@
 console.log('E-Commerce API');
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+//database
+const connectDB = require('./db/connect')
+
+const start = async()=> {
+   
+    try {
+        await connectDB(process.env.MONGO_URI) 
+        app.listen(port, console.log(`server is listening on ${port}...`))
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+
+
+start()
